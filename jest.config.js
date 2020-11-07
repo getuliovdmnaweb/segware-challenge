39d6671871c57e1715269589dd46d972002c0e7f
Getulio Vieira Desktop <getuliovdmnaweb@gmail.com>
2020-11-07 17:39:48 -0300

    Configuring React Navigation on tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 const { withEnzyme } = require("jest-expo-enzyme");
 
 module.exports = {
+  preset: "react-native",
+  setupFiles: ["./jest/setup.js"],
   projects: [
     // Skipping Node because we want to test DOM presets only
     withEnzyme(require("jest-expo/ios/jest-preset")),
